c78d4457efe2960a5b1a9a0f997806feaff486fa
Rodrigo Salado Anaya <rodrigo.salado@knowtion.biz>
2012-09-10 14:39:08 -0500

    Terminado solo falta afinar detalles visuales.

--- Runner/src/runner/Runner.java
+++ Runner/src/runner/Runner.java
@@ -1,7 +1,12 @@
 package runner;
 
 import game.Play;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.AnimationTimer;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
@@ -17,44 +22,81 @@ public class Runner extends Application {
     private static final boolean DEAD = false;
     private static final char CELL_LIVE_CHAR = '*';
     private static final char CELL_DEAD_CHAR = '-';
+    private Timeline timeline;
+    private AnimationTimer timer;
+    private static Play p = new Play();
+    private static int LENGTH;
 
-    /**
-     * @param args the command line arguments
-     */
+    private void init(Stage primaryStage) {
+        Group root = new Group();
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 100, 100));
+        final Text text = new Text("".toString());
+        iniBoard();
+        final StackPane stack = new StackPane();
+        stack.getChildren().addAll(text);
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.setAutoReverse(true);
+
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                text.setText(getText());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                }
+            }
+        };
+        root.getChildren().add(stack);
+    }
+
+    public void play() {
+        timeline.play();
+        timer.start();
+    }
+
+    @Override
+    public void stop() {
+        timeline.stop();
+        timer.stop();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        init(primaryStage);
+        primaryStage.show();
+        play();
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Game of life");
-        Text text = new Text();
-        text.setText(getText());
-        StackPane root = new StackPane();
-        root.getChildren().add(text);
-        primaryStage.setScene(new Scene(root, 120, 120));
-        primaryStage.show();
+    public void iniBoard() {
+        
+        boolean[][] initialBoard = {
+            {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+            {DEAD, DEAD, LIVE, LIVE, DEAD, DEAD},
+            {DEAD, LIVE, DEAD, DEAD, DEAD, DEAD},
+            {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+            {DEAD, DEAD, DEAD, LIVE, DEAD, DEAD},
+            {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}};
+        LENGTH = initialBoard.length;
+        p.setLength(LENGTH);
+        p.fillBoard(initialBoard);
     }
 
     public String getText() {
-        Play p = new Play();
-        boolean[][] initialBoard = {
-            {DEAD, DEAD, DEAD, DEAD, LIVE, DEAD},
-            {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD},
-            {DEAD, LIVE, DEAD, DEAD, LIVE, DEAD},
-            {DEAD, LIVE, DEAD, DEAD, LIVE, DEAD},
-            {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD},
-            {DEAD, DEAD, DEAD, DEAD, LIVE, DEAD}};
-        int length = initialBoard.length;
-        p.setLength(length);
-        p.fillBoard(initialBoard);
+
 
         p.nextExolution();
         boolean[][] board = p.getActualBoard();
         String boardStr = "";
 
-        for (int i = 0; i != length; i++) {
-            for (int j = 0; j != length; j++) {
+        for (int i = 0; i != LENGTH; i++) {
+            for (int j = 0; j != LENGTH; j++) {
                 boardStr += board[i][j] ? CELL_LIVE_CHAR : CELL_DEAD_CHAR;
             }
             boardStr += "\n";
